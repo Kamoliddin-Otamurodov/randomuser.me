6483fe62bdaefffd203438c1b256024741d563ca
Kamoliddin-Otamurodov <kolik2125@gmail.com>
2024-07-16 10:55:01 +0500

    init

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from randomuser import RandomUser
 
-user = RandomUser()
-# 
+user = RandomUser("https://randomuser.me/api/")
+ 
 print(type(user))
 print(user.get_first_name()) 
+print(user.get_last_name())
+print(user.get_gender())
+print(user.get_country())
+print(user.get_email())
--- randomuser.py
+++ randomuser.py
@@ -1,3 +1,29 @@
+import requests
 class RandomUser:
-    def __init__(self):
-        pass
+    def __init__(self , url):
+        self.url = url
+
+    def get_first_name(self):
+        response = requests.get(self.url)
+        data = response.json()
+        return data["results"][0]["name"]["first"]
+
+    def get_last_name(self):
+        response = requests.get(self.url)
+        data = response.json()
+        return data["results"][0]["name"]["last"]
+
+    def get_gender(self):
+        response = requests.get(self.url)
+        data = response.json()
+        return data["results"][0]["gender"]
+
+    def get_country(self):
+        response = requests.get(self.url)
+        data = response.json()
+        return data["results"][0]["location"]["country"]
+
+    def get_email(self):
+        response = requests.get(self.url)
+        data = response.json()
+        return data["results"][0]["email"]
